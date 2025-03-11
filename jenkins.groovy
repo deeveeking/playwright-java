83cbf440b788b04a7231053f6f0b035b2d33e29e
deeveeking <thisformyworld@gmail.com>
2025-03-12 00:32:05 +0200

    update jenkins file

--- jenkins.groovy
+++ jenkins.groovy
@@ -1,9 +1,8 @@
-pipline {
+node {
     stages {
         parameters {
             booleanParam 'isUserNeedToBeLogin'
         }
-
         stage('Execute shell script for running test') {
             steps {
                 echo 'Executing shell script'
