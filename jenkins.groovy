24568aefff9a8df40b97b09915a22339d4b6c633
deeveeking <thisformyworld@gmail.com>
2025-03-12 01:05:05 +0200

    update jenkins file

--- jenkins.groovy
+++ jenkins.groovy
@@ -4,7 +4,7 @@ node {
     try {
         stage("Run Test") {
             echo "Var value = ${isUserNeedToBeLogin}"
-            if (isUserNeedToBeLogin) {
+            if (${isUserNeedToBeLogin}) {
                 labelledShell(label: "Run default test", script: "chmod +x gradlew \n./gradlew clean loginTest -DisUserNeedToBeLogin=${isUserNeedToBeLogin}")
             } else {
                 labelledShell(label: "Run Login test", script: "chmod +x gradlew \n./gradlew clean test -DisUserNeedToBeLogin=${isUserNeedToBeLogin}")
