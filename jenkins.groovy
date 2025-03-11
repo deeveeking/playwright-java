6b3e51ec7224b191d2f245e9233682b0fd025ed9
deeveeking <thisformyworld@gmail.com>
2025-03-12 00:55:08 +0200

    update jenkins file

--- jenkins.groovy
+++ jenkins.groovy
@@ -1,9 +1,27 @@
+isUserNeedToBeLogin = "${isUserNeedToBeLogin}"
+
 node {
-    parameters {
-        booleanParam 'isUserNeedToBeLogin'
+    try {
+        stage("Run Test") {
+            echo "Var value = ${isUserNeedToBeLogin}"
+            if (isUserNeedToBeLogin) {
+                labelledShell(label: "Run default test", script: "chmod +x gradlew \n./gradlew clean loginTest -DisUserNeedToBeLogin=${isUserNeedToBeLogin}")
+            } else {
+                labelledShell(label: "Run Login test", script: "chmod +x gradlew \n./gradlew clean test -DisUserNeedToBeLogin=${isUserNeedToBeLogin}")
+            }
+        }
+    } finally {
+        generateAllure()
+        echo "Some fails..."
     }
-    stage('Execute shell script for running test') {
-        echo 'Executing shell script'
-        sh "chmod u+x ./script.sh;./script.sh ${params.isUserNeedToBeLogin}"
-    }
+}
+
+def generateAllure() {
+    allure([
+            includeProperties: true,
+            jdk: '',
+            properties: [],
+            reportBuildPolice: 'ALWAYS',
+            results: [[path: 'build/allure-results']]
+    ])
 }
--- script.sh
+++
@@ -1,9 +0,0 @@
-chmod +x gradlew
-echo "Var value = ${isUserNeedToBeLogin}"
-if [ $isUserNeedToBeLogin = true ]
-then
-	./gradlew clean test -DisUserNeedToBeLogin=${isUserNeedToBeLogin}
-else
-	echo "Else block"
-	./gradlew clean loginTest -DisUserNeedToBeLogin=${isUserNeedToBeLogin}
-fi
