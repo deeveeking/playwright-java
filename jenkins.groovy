88541e70575ea275c51815f25154392f91e342b5
deeveeking <thisformyworld@gmail.com>
2025-03-12 00:33:17 +0200

    update jenkins file

--- jenkins.groovy
+++ jenkins.groovy
@@ -1,13 +1,11 @@
 node {
-    stages {
-        parameters {
-            booleanParam 'isUserNeedToBeLogin'
-        }
-        stage('Execute shell script for running test') {
-            steps {
-                echo 'Executing shell script'
-                sh "chmod u+x $WORKSPACE/script.sh;$WORKSPACE/script.sh ${params.isUserNeedToBeLogin}"
-            }
+    parameters {
+        booleanParam 'isUserNeedToBeLogin'
+    }
+    stage('Execute shell script for running test') {
+        steps {
+            echo 'Executing shell script'
+            sh "chmod u+x $WORKSPACE/script.sh;$WORKSPACE/script.sh ${params.isUserNeedToBeLogin}"
         }
     }
 }
